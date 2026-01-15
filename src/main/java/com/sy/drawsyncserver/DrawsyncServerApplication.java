a19386b9402c99c0199a1fb44b648c7843f76213
yoon9531 <dlatjddbs9844@yonsei.ac.kr>
2026-01-15 18:16:47 +0900

    Initial commit

---
+++ src/main/java/com/sy/drawsyncserver/DrawsyncServerApplication.java
@@ -0,0 +1,13 @@
+package com.sy.drawsyncserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawsyncServerApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(DrawsyncServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sy/drawsyncserver/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.sy.drawsyncserver.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic/"); // 클라이언트로 메시지를 보낼 때
+        registry.setApplicationDestinationPrefixes("/app"); // 클라이언트에서 메시지를 보낼 때
+    }
+}
